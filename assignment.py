1bc7ba6e409428532e1935d00917ff6e6ab048a3
antijun <68279777+antijun@users.noreply.github.com>
2020-10-13 13:03:17 -0700

    update

--- assignment.py
+++ assignment.py
@@ -20,33 +20,32 @@ def factorPair(number, factor):
 
 def cosineLaw(side1, side2, angle, oppositeSide=True):
     import math
-    x = convertAngle(angle)
+    x = toRadians(angle)
     if oppositeSide == True:
-        answer = math.sqrt(side1**2 + side2**2 - 2*side1*side2*math.cos(x))
+        math.sqrt(side1**2 + side2**2 - 2*side1*side2*math.cos(x))
     elif oppositeSide == False:
         sides = (side1, side2)
-        y = math.sin(min(sides), max(sides))
-    return y
+        math.sqrt( max(sides)**2 - min(sides)**2 )
+    return 
 
 
-def convertAngle(angle):
+def toRadians(angle):
     import math
     radianAngle = angle*(math.pi/180)
     x = float(radianAngle)
     return x
 
-
-def solution(a, b):
-    a = answers[0]
-    b = answers[1]
-    x = max[a, b]
-    return x
-
-
 def quadratic(a, b, c):
     import math
     x1 = (-b + math.sqrt(b**2 - 4*a*c))/(2*a)
     x2 = (-b - math.sqrt(b**2 - 4*a*c))/(2*a)
-    answers = float([x1, x2])
+    answers = [x1, x2]
     answers.sort()
     return answers
+
+def solution( a ):
+    z = max(a)
+    return z
+
+
+
--- assignment_test.py
+++ assignment_test.py
@@ -1,33 +1,36 @@
 #! python3
 
-import assignment
-
-def test1():
-  assert assignment.function() == expected output
-
-def test2():
-  assert "needle" in assignment.function()
-  assert "needle2" in assignment.function(),"error Message"
 #!python3
 import assignment
 
+
 def test1():
-  x = assignment.toRadians(30)
-  assert round(x,4) == 0.5236
+    x = assignment.toRadians(30)
+    assert round(x, 4) == 0.5236
+
 
 def test2():
-  x = assignment.solution([-8.9, 5.3])
-  assert x == 5.3
+    x = assignment.solution([-8.9, 5.3])
+    assert x == 5.3
+
 
 def test3():
-  x1,x2 = assignment.quadratic(3,5,-8)
-  assert round(x1,2) == -2.67
-  assert round(x2,2) == 1
+    x1, x2 = assignment.quadratic(3, 5, -8)
+    assert round(x1, 2) == -2.67
+    assert round(x2, 2) == 1
+
 
 def test4():
-  answer = assignment.cosineLaw(6,9,34)
-  assert round(answer,1) == 5.2
+    answer = assignment.cosineLaw(6, 9, 34)
+    assert round(answer, 1) == 5.2
+
 
 def test5():
-  answer = assignment.cosineLaw(10,3,50,oppositeSide=False)
-  assert round(answer,1) == 11.7 
+    answer = assignment.cosineLaw(10, 3, 50, oppositeSide=False)
+    assert round(answer, 1) == 11.7
+
+
+test1()
+test2()
+test3()
+test4()
